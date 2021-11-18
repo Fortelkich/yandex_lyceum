3d07ae51461b1f41cb1e8130da577e6d0dcd6683
Fortelkich <94623278+Fortelkich@users.noreply.github.com>
2021-11-18 18:47:16 +0300

    Version Different Colors

--- Git_and_yellow_circles.py
+++ Git_and_yellow_circles.py
@@ -1,17 +1,15 @@
-from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QLabel, QGridLayout
+from PyQt5.QtWidgets import QMainWindow
 import random
-from PyQt5.QtWidgets import QWidget, QApplication
-from PyQt5.QtGui import QPainter, QColor, QPen
-from PyQt5.QtCore import Qt
-from tkinter import Tk, Canvas, Frame, BOTH
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QPainter, QColor
 import sys
+from Git_and_yellow_circles1 import Ui_Dialog
 
 
-class show_receipt(QMainWindow, QDialog):
+class show_receipt(QMainWindow, Ui_Dialog):
     def __init__(self, parent=None):
         super().__init__()
-        uic.loadUi('Git_and_yellow_circles.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
 
@@ -29,12 +27,15 @@ class show_receipt(QMainWindow, QDialog):
     def drawRectangles(self, qp):
         razmer = random.randint(1, 500)
         mesto = random.randint(1, 50)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
 
         col = QColor(0, 0, 0)
         col.setNamedColor('#d4d4d4')
         qp.setPen(col)
 
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(r, g, b))
         qp.drawEllipse(mesto, mesto, razmer, razmer)
 
 
